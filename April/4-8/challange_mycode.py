1bf1b2b19c9c09d6329531ba2697d17dbd8f781c
Kasun Gimantha <97676779+KasunGimantha@users.noreply.github.com>
2025-04-08 22:37:40 +0530

    Auto commit - Tue 04/08/2025 22:37:40.55

---
+++ April/4-8/challange_mycode.py
@@ -0,0 +1,31 @@
+# method 1
+
+# name
+name = input("Input your name: ")
+# age
+age = input("Input your age: ")
+# height
+height = input("Input your height in cm: ")
+
+print("Hi ", name, "! You are ", str(age),
+      " years old and ", float(height)/100, " meters tall")
+# optional suggestion
+# print(f"Hi {name}! You are {age} years old and {float(height) / 100:.2f} meters tall.")
+
+
+# method 2
+# (!failed)
+# name = ""
+# age=""
+# height=""
+
+# def person_detail(name,age,height):
+#    return print("Hi ",name,"! You are ",str(age)," years old and ",float(height)/100," meters tall")
+
+# values = [name,age,height]
+# for x in values:
+#     x = input("Enter your ")
+
+# #person_detail(name,age,height)
+
+# print(values)
